bcbc5cab23f4898285f72b75b80e8544012aa185
MetalSquid <bslowinskird@gmail.com>
2023-12-31 17:20:23 -0500

    setup initial state and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PlaylistComponent from './Components/Playlist/Playlist'; 
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <main className='App-Main'> 
+      <PlaylistComponent />
+      </main> 
     </div>
   );
 }
---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function PlaylistComponent() {
+    return (
+        <h1>Playlist Goes Here</h1>
+    )
+}
